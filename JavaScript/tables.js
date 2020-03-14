64f15f4f05ac757d13b3cf0dcee81748c7977510
Oana <sabouoana@gmail.com>
2020-03-14 18:55:06 +0200

    - validate the pass

--- JavaScript/tables.js
+++ JavaScript/tables.js
@@ -1,7 +1,7 @@
 let userAdded = new Array();
 let createUser = new Array();
-userAdded[0]="0";
-userAdded[1]="0";
+userAdded[0] = "0";
+userAdded[1] = "0";
 // $(document).ready(function () {
 //     $.ajax({
 //         type: "GET",
@@ -32,7 +32,7 @@ function getUsers() {
 
 function SaveUser() {
     userAdded[0] = document.getElementById("idUser").value;
-userAdded[1] = document.getElementById("userPassword").value;
+    userAdded[1] = document.getElementById("userPassword").value;
     // $.ajax({
     //     type: "POST",
     //     url: "/SaveUser",
@@ -44,21 +44,37 @@ userAdded[1] = document.getElementById("userPassword").value;
 }
 
 function createUserFunction() {
-        createUser[0] = document.getElementById("firstName").value;
-        createUser[1] = document.getElementById("secondName").value;
-        createUser[2] = document.getElementById("userEmail").value;
-        createUser[3] = document.getElementById("vehicle").value;
-        createUser[4] = document.getElementById("createPsw").value;
-        createUser[5] = document.getElementById("re-enterPsw").value;
-        
-        $.ajax({
-            type: "POST",
-            url: "/createUser",
-            data: {user:createUser},
-            success: function(data) {
-               // console.log(data);
-            }
-        });
- 
+    createUser[0] = document.getElementById("firstName").value;
+    createUser[1] = document.getElementById("secondName").value;
+    createUser[2] = document.getElementById("userEmail").value;
+    createUser[3] = document.getElementById("vehicle").value;
+    createUser[4] = document.getElementById("createPsw").value;
+    createUser[5] = document.getElementById("re-enterPsw").value;
+
+    validatePassword(createUser[4]);
+
+    // if (createUser[4] != createUser[5]) {
+    //     alert("Password does not match.");
+    // }
+
+
+
+    $.ajax({
+        type: "POST",
+        url: "/createUser",
+        data: { user: createUser },
+        success: function(data) {
+            // console.log(data);
+        }
+    });
+
     console.log(createUser);
+}
+
+function validatePassword(pass) {
+    let psw = pass.length;
+    console.log(psw);
+    if (psw < 6) {
+        alert("The length is too small.");
+    }
 }
