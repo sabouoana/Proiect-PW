d889d735636af20732d85681358ef0e9aa8a9af0
Oana <sabouoana@gmail.com>
2020-03-18 17:47:00 +0200

    - tabs

--- Server.js
+++ Server.js
@@ -1,7 +1,7 @@
 const express = require('express');
-const app=express();
-const api=require('./serverside/mess-api');
-const bodyParser=require('body-parser');
+const app = express();
+const api = require('./serverside/mess-api');
+const bodyParser = require('body-parser');
 
 const MongoClient = require('mongodb').MongoClient;
 const uri = "mongodb+srv://dbAdmin:dbAdmin@cluster0-vaxrd.mongodb.net/test";
@@ -12,39 +12,43 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-app.listen(3000,() =>{
-	console.log('listening on 3000');
+app.listen(3000, () => {
+    console.log('listening on 3000');
 });
-    
+
 
 client.connect(err => {
-  // perform actions on the collection object
-  if(err)
-	throw err;
-  console.log('Connection works!');
+    // perform actions on the collection object
+    if (err)
+        throw err;
+    console.log('Connection works!');
 });
 
 
-app.use('/JavaScript',express.static('JavaScript'));
-app.use('/img',express.static('img'));
+app.use('/JavaScript', express.static('JavaScript'));
+app.use('/img', express.static('img'));
 
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
     res.sendFile(__dirname + '/main.html');
 });
 
-app.get('/user',(req,res)=>{
-  res.sendFile(__dirname + '/user.html');
+app.get('/user', (req, res) => {
+    res.sendFile(__dirname + '/user.html');
 });
 
-app.get('/admin',(req,res)=>{
-  res.sendFile(__dirname + '/admin.html');
+app.get('/admin', (req, res) => {
+    res.sendFile(__dirname + '/admin.html');
 });
 
-app.get('/GetUser',(req,res)=>{
-  api.getUsers(client,res);
+app.get('/create-user', (req, res) => {
+    res.sendFile(__dirname + '/create-user.html');
 });
 
-app.post('/createUser',function(req,res){
-    api.createUser(client,req.body.user,res);
+app.get('/GetUser', (req, res) => {
+    api.getUsers(client, res);
+});
+
+app.post('/createUser', function(req, res) {
+    api.createUser(client, req.body.user, res);
     res.send("Save was successful!");
 });
